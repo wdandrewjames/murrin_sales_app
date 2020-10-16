38a1b5abd35f01f1148b186362d0c95d309ee428
wdandrewjames <wd.andrew.james@gmail.com>
2020-10-16 19:34:25 +0100

    Minified app.css and app.js and purged CSS

--- resources/views/business/show.blade.php
+++ resources/views/business/show.blade.php
@@ -17,8 +17,9 @@
                 <x-table>
                     <x-slot name="head">
                         <x-head x-on:click="open = !open">Name</x-head>
-                        <x-head>Age</x-head>
                         <x-head>Email</x-head>
+                        <x-head>Website</x-head>
+                        <x-head>Tel</x-head>
                         <x-head>Status</x-head>
                     </x-slot>
                     <x-slot name="body">
@@ -27,6 +28,7 @@
                                 <td class="px-6 py-4 whitespace-no-wrap"><a href="{{ route('customer.show', $customer->id) }}">{{ $customer->name }}</a></td>
                                 <td class="px-6 py-4 whitespace-no-wrap">{{ $customer->email }}</td>
                                 <td class="px-6 py-4 whitespace-no-wrap">{{ $customer->website }}</td>
+                                <td class="px-6 py-4 whitespace-no-wrap">{{ $customer->tel }}</td>
                                 <td class="px-6 py-4 whitespace-no-wrap">
                                     <span title="{{ $customer->status->description }}" class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-{{ $customer->status->color }}-100 text-{{ $customer->status->color }}-800">
                                         {{ $customer->status->name }}
---
+++ resources/views/classes.blade.php
@@ -0,0 +1,5 @@
+<div class="bg-gray-100 text-gray-800"></div>
+<div class="bg-red-100 text-red-800"></div>
+<div class="bg-green-100 text-green-800"></div>
+<div class="bg-orange-100 text-orange-800"></div>
+<div class="bg-purple-100 text-purple-800"></div>
