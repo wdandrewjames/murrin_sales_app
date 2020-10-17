89393b75dbfb827525a49b05da014aa5a9871f90
wdandrewjames <wd.andrew.james@gmail.com>
2020-10-17 14:14:28 +0100

    Change scheduled commands to test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -25,7 +25,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         $schedule->command('summary:create')->lastDayOfMonth('23:55');
-        $schedule->command('send_appointment:email')->saturdays('14:12');
+        $schedule->command('send_appointment:email')->saturdays()->at('14:17');
     }
 
     /**
