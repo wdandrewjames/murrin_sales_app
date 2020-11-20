5c21cfe11e783fb1a6826bbfe5c48f191ad70655
wdandrewjames <wd.andrew.james@gmail.com>
2020-11-20 10:20:26 +0000

    Changed times emails go out

--- app/Console/Commands/GenerateReport.php
+++ app/Console/Commands/GenerateReport.php
@@ -119,7 +119,7 @@ class GenerateReport extends Command
             ];
 
             $pdf = \PDF::loadView('pdf', $data)->setPaper('A4', 'landscape')->stream();
-            Mail::to($business->email)->send(new \App\Mail\SendReport($pdf, $business));
+            Mail::to(['markmurrin@mail.com', $business->email])->send(new \App\Mail\SendReport($pdf, $business));
         });
 
     }
